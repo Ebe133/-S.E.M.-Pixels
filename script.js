76603c27cb115d4c1c334517a87e5e94019f881e
SanderHoudijk <fristislekker@proton.me>
2024-10-15 14:47:31 +0200

    Upload Script.js with Psuedo Code

---
+++ script.js
@@ -0,0 +1,28 @@
+// Hier zet je neer hoeveel lives je hebt.
+// Hier komt een variable voor true/false voor of je het schild hebt.
+// Hier komt een variabele voor de score. Deze begint op 0
+// Hier komt een variabel voor je topscore, deze update pas als hij verbroken is
+// Hier komt een variabele om aan te geven of de game gestart is. 
+
+// Hier komt de code voor het starten van de game bij het drukken van een toets.
+
+// Hier komt de code te staan voor het omhoog gaan met spatie.
+
+// Hier komt de code voor het omlaag vallen.
+
+// Hier wordt het systeem gemaakt, dat de obstakels inlaad met een bepaalde afstand er tussen.
+
+// Hier wordt een script geschreven, dat powerups inlaad. Als je die aanraakt, word de bepaalde variable aangepast.
+
+// Hier komt de code voor het toevoegen van de score. Dit gebeurd elke seconde.
+
+// Hier komt een stuk code, wat kijkt of de highscore verbeterd is. Als dat zo is, dan update hij het variable.
+
+// Hier komt code om te kijken wanneer je de muur of grond raakt, dat laat het "Game Over" scherm komen.
+// Op het game over scherm staat je huidige score en je highscore.
+// Hier komt een stuk om te checken of je een schild, of een extra leven hebt. Als dat zo is, ga je niet af.
+// In dit scherm staat een knop om opnieuw te spelen, en een knop om naar het leaderboard te gaan.
+
+// Hier komt de code, om de highscores van hoog, naar laag te sorteren en in een table te zetten.
+
+// Hier komt de code, om de palen naar je toe te laten komen. Dit gebeurd alleen als het "gameStarted" variable true is.
